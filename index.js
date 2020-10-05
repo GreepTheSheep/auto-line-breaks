878b2b5396278e30297b18d6b82fda6b6e9d785b
Matthieu <89matt89.md@gmail.com>
2020-10-05 09:18:25 +0200

    1.0.2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = function(text, lengthLine){
     var lines = []
     lines.push(text)
     var i = 0
+    if (lengthLine <= 0) throw console.error('Length must NOT be lower than 1');
     while (text.length >= lengthLine * i+1) {
         if (lines[i].charAt(lengthLine-1) == ' '){
             lines.push(text.slice(lengthLine*i, lengthLine*i + lengthLine-1).trim())
@@ -9,7 +10,7 @@ module.exports = function(text, lengthLine){
             if (lines[i].charAt(lengthLine) == ' '){
                 lines.push(text.slice(lengthLine*i, lengthLine*i + lengthLine))
             } else {
-                if (text.slice(lengthLine*i, lengthLine*i + lengthLine).length < lengthLine){
+                if (text.slice(lengthLine*i, lengthLine*i + lengthLine).length <= lengthLine){
                     lines.push(text.slice(lengthLine*i, lengthLine*i + lengthLine).trim())
                 } else {
                     lines.push(text.slice(lengthLine*i, lengthLine*i + lengthLine).concat('-').trim())
